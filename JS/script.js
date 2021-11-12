2140e0bc8e2bd3a25b475d7492b4a31118ff03b2
Ghostie333 <kamilzak886@gmail.com>
2021-11-13 00:43:30 +0100

    PSI-Zadanie zmodyfikowane

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,8 @@ window.onload = function() {
 
     const randomValues = document.getElementsByClassName("randomValues")
     const reverseValues = document.getElementsByClassName("reverseValues")
+    const przel = document.getElementById("przel")
+    const ukryj = document.getElementById("ukryj")
 
     for(let i=0 ; i<randomValues.length ; i++){
         randomValues[i].value = (Math.random()*100+1)
@@ -10,7 +12,8 @@ window.onload = function() {
         reverseValues[i].value = reverseValues.length-i
     }
 
+    przel.onclick = () =>{
+        ukryj.classList.toggle("hidden")
+    }
 
-
-    
 }

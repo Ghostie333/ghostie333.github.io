6e5b838730d1cbdbf3e9f86e92c24512611fe5ce
Ghostie333 <kamilzak886@gmail.com>
2021-11-13 12:53:57 +0100

    PSI-Zadanie

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,8 @@ window.onload = function() {
 
     const randomValues = document.getElementsByClassName("randomValues")
     const reverseValues = document.getElementsByClassName("reverseValues")
-    const przel = document.getElementById("przel")
-    const ukryj = document.getElementById("ukryj")
+    const przel = document.getElementsByClassName("przel")
+    const ukryj = document.getElementsByClassName("ukryj")
 
     for(let i=0 ; i<randomValues.length ; i++){
         randomValues[i].value = (Math.random()*100+1)
@@ -11,9 +11,11 @@ window.onload = function() {
     for(let i=0 ; i<reverseValues.length ; i++){
         reverseValues[i].value = reverseValues.length-i
     }
-
-    przel.onclick = () =>{
-        ukryj.classList.toggle("hidden")
+    for(let i=0 ; i < przel.length ; i++){
+        przel[i].onclick = () =>{
+            ukryj[i].classList.toggle("hidden")
+        }
     }
+    
 
 }
